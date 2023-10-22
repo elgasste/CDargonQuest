ae3fdab1f10ef6eab452e42541802ec7a17aab8b
selgas <s.elgas@techsmith.com>
2023-10-22 16:33:30 -0400

    Make the title menu global
    This matches the current state of everything else, why not make menus global as well?

--- CDargonQuest/title_input_handler.c
+++ CDargonQuest/title_input_handler.c
@@ -2,20 +2,20 @@
 #include "title_input_handler.h"
 #include "input_state.h"
 #include "event_queue.h"
-#include "game.h"
+#include "menu.h"
 
 void dqTitleInputHandler_HandleInput()
 {
    if ( dqInputState_WasKeyPressed( sfKeyDown ) )
    {
-      dqMenu_ScrollDown( dqGame->titleMenu );
+      dqMenu_ScrollDown( dqTitleMenu );
    }
    else if ( dqInputState_WasKeyPressed( sfKeyUp ) )
    {
-      dqMenu_ScrollUp( dqGame->titleMenu );
+      dqMenu_ScrollUp( dqTitleMenu );
    }
    else if ( dqInputState_WasKeyPressed( sfKeyReturn ) )
    {
-      dqEventQueue_Push( dqGame->titleMenu->options[dqGame->titleMenu->selectedOption].eventType );
+      dqEventQueue_Push( dqTitleMenu->options[dqTitleMenu->selectedOption].eventType );
    }
 }
