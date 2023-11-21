c939f55c1ebfaedf7b3c38a2a09ce3d304db8ec9
selgas <s.elgas@techsmith.com>
2023-11-21 09:33:34 -0500

    Added a few more enemy templates

--- CDargonQuest/game_data.c
+++ CDargonQuest/game_data.c
@@ -46,7 +46,7 @@ static void dqGameData_TempLoadEnemyTemplates()
 {
    unsigned int i;
 
-   dqGameData->enemyTierCount = 2;
+   dqGameData->enemyTierCount = 4;
    dqGameData->enemyTemplateCount = 2;
    dqGameData->enemyTemplates = (dqEnemyTemplate_t**)dqMalloc( sizeof( dqEnemyTemplate_t* ) *
                                                                (size_t)( dqGameData->enemyTierCount ),
@@ -59,7 +59,7 @@ static void dqGameData_TempLoadEnemyTemplates()
                                                                     sfTrue );
    }
 
-   // TODO: don't worry about the graphics for now, just generate some data to test with
+   // tier 0
    sprintf_s( dqGameData->enemyTemplates[0][0].name, ENTITY_NAME_SIZE, "slime" );
    dqGameData->enemyTemplates[0][0].indefiniteArticle = dqIndefiniteArticleA;
    dqGameData->enemyTemplates[0][0].minHitPoints = 4;
@@ -74,6 +74,7 @@ static void dqGameData_TempLoadEnemyTemplates()
    dqGameData->enemyTemplates[0][1].attackPower = 3;
    dqGameData->enemyTemplates[0][1].defensePower = 3;
 
+   // tier 1
    sprintf_s( dqGameData->enemyTemplates[1][0].name, ENTITY_NAME_SIZE, "horned rabbit" );
    dqGameData->enemyTemplates[1][0].indefiniteArticle = dqIndefiniteArticleA;
    dqGameData->enemyTemplates[1][0].minHitPoints = 8;
@@ -87,4 +88,50 @@ static void dqGameData_TempLoadEnemyTemplates()
    dqGameData->enemyTemplates[1][1].maxHitPoints = 14;
    dqGameData->enemyTemplates[1][1].attackPower = 7;
    dqGameData->enemyTemplates[1][1].defensePower = 3;
+
+   // tier 2
+   sprintf_s( dqGameData->enemyTemplates[2][0].name, ENTITY_NAME_SIZE, "babble" );
+   dqGameData->enemyTemplates[2][0].indefiniteArticle = dqIndefiniteArticleA;
+   dqGameData->enemyTemplates[2][0].minHitPoints = 10;
+   dqGameData->enemyTemplates[2][0].maxHitPoints = 14;
+   dqGameData->enemyTemplates[2][0].attackPower = 8;
+   dqGameData->enemyTemplates[2][0].defensePower = 5;
+
+   sprintf_s( dqGameData->enemyTemplates[2][1].name, ENTITY_NAME_SIZE, "froggore" );
+   dqGameData->enemyTemplates[2][1].indefiniteArticle = dqIndefiniteArticleA;
+   dqGameData->enemyTemplates[2][1].minHitPoints = 10;
+   dqGameData->enemyTemplates[2][1].maxHitPoints = 12;
+   dqGameData->enemyTemplates[2][1].attackPower = 6;
+   dqGameData->enemyTemplates[2][1].defensePower = 5;
+
+   // TODO: figure out how to have different numbers of enemies per tier
+   /*sprintf_s( dqGameData->enemyTemplates[2][2].name, ENTITY_NAME_SIZE, "scorpion wasp" );
+   dqGameData->enemyTemplates[2][2].indefiniteArticle = dqIndefiniteArticleA;
+   dqGameData->enemyTemplates[2][2].minHitPoints = 10;
+   dqGameData->enemyTemplates[2][2].maxHitPoints = 14;
+   dqGameData->enemyTemplates[2][2].attackPower = 6;
+   dqGameData->enemyTemplates[2][2].defensePower = 5;*/
+
+   // tier 3
+   sprintf_s( dqGameData->enemyTemplates[3][0].name, ENTITY_NAME_SIZE, "healer" );
+   dqGameData->enemyTemplates[3][0].indefiniteArticle = dqIndefiniteArticleA;
+   dqGameData->enemyTemplates[3][0].minHitPoints = 25;
+   dqGameData->enemyTemplates[3][0].maxHitPoints = 32;
+   dqGameData->enemyTemplates[3][0].attackPower = 6;
+   dqGameData->enemyTemplates[3][0].defensePower = 6;
+
+   sprintf_s( dqGameData->enemyTemplates[3][1].name, ENTITY_NAME_SIZE, "masked moth" );
+   dqGameData->enemyTemplates[3][1].indefiniteArticle = dqIndefiniteArticleA;
+   dqGameData->enemyTemplates[3][1].minHitPoints = 12;
+   dqGameData->enemyTemplates[3][1].maxHitPoints = 15;
+   dqGameData->enemyTemplates[3][1].attackPower = 7;
+   dqGameData->enemyTemplates[3][1].defensePower = 5;
+
+   // TODO: see above
+   /*sprintf_s( dqGameData->enemyTemplates[3][2].name, ENTITY_NAME_SIZE, "magician" );
+   dqGameData->enemyTemplates[3][2].indefiniteArticle = dqIndefiniteArticleA;
+   dqGameData->enemyTemplates[3][2].minHitPoints = 15;
+   dqGameData->enemyTemplates[3][2].maxHitPoints = 20;
+   dqGameData->enemyTemplates[3][2].attackPower = 7;
+   dqGameData->enemyTemplates[3][2].defensePower = 7;*/
 }
